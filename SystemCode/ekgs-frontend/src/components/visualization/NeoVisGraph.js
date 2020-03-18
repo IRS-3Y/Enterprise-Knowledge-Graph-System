c56f836e70722916f4f8ed74b8f4e861ead2cf78
TS, Yin <tianshi.yin@gmail.com>
2020-03-18 23:37:04 +0800

    Add neovis.js

---
+++ SystemCode/ekgs-frontend/src/components/visualization/NeoVisGraph.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import NeoVis, {NEOVIS_DEFAULT_CONFIG} from 'neovis.js/dist/neovis.js';
+
+const useStyles = makeStyles(theme => ({
+  NeoVisGraph: {
+    width: "100%",
+    height: "70vh",
+    font: "22pt arial"
+  }
+}));
+
+export default function NeoVisGraph({cypher}) {
+  useEffect(() => {
+    new NeoVis({
+      container_id: "viz",
+      server_url: "bolt://dev.vm:7687",
+      labels: {
+        [NEOVIS_DEFAULT_CONFIG]: {
+          caption: "name"
+        }
+      },
+      relationships: {
+        [NEOVIS_DEFAULT_CONFIG]: {
+          caption: false
+        }
+      },
+      initial_cypher: cypher
+    })
+    .render();
+  });
+
+  const classes = useStyles();
+  return <div id="viz" className={classes.NeoVisGraph}/>
+}
--- SystemCode/ekgs-frontend/src/views/Dashboard.js
+++ SystemCode/ekgs-frontend/src/views/Dashboard.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { components } from '@lib/material-dashboard-react';
+import NeoVisGraph from '../components/visualization/NeoVisGraph';
+import ErrorBoundary from '../components/core/ErrorBoundary';
+import { makeStyles } from '@material-ui/core';
+
+const {Card, CardHeader, CardBody, CardFooter, Input} = components;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: 0
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  },
+  cardSubtitleWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+}));
 
 export default function Dashboard() {
+  const classes = useStyles();
+  const title = "Cypher Query";
+  const subtitle = null;
+  const color ="info";
+  const [cypher, setCypher] = useState("MATCH(n)-[r]->(m) RETURN *");
   return (
-    <div>
-      <p>Demo Page</p>
-    </div>
+    <Card className={classes.root}>
+      <CardHeader color={color}>
+        {title? <h4 className={classes.cardTitleWhite}>{title}</h4>: null}
+        {subtitle? <p className={classes.cardSubtitleWhite}>{subtitle}</p>: null}
+      </CardHeader>
+      <CardBody>
+        <ErrorBoundary>
+          <NeoVisGraph cypher={cypher}/>
+        </ErrorBoundary>
+      </CardBody>
+      <CardFooter>
+        <Input labelText="Query" 
+          formControlProps={{fullWidth: true}}
+          inputProps={{
+            value: cypher,
+            onChange: e => setCypher(e.target.value)
+          }}/>
+      </CardFooter>
+    </Card>
   );
 }
