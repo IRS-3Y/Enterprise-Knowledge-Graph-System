9a98957cb4a5a3c28c1da73d803c511d70035300
TS, Yin <tianshi.yin@gmail.com>
2020-04-02 14:54:43 +0800

    Add backend rules for search bar

--- SystemCode/ekgs-backend/src/main/java/mtech/irs/ekgs/model/SearchResults.java
+++ SystemCode/ekgs-backend/src/main/java/mtech/irs/ekgs/model/SearchResults.java
@@ -2,6 +2,7 @@ package mtech.irs.ekgs.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import lombok.Data;
 
@@ -14,4 +15,21 @@ import lombok.Data;
 public class SearchResults {
 
 	private List<SearchResult> results = new ArrayList<>();
+	
+	public void add(SearchResult result) {
+		results.add(result);
+	}
+	
+	public void addSuggestion(String value) {
+		SearchSuggestion result  = new SearchSuggestion();
+		result.setValue(value);
+		results.add(result);
+	}
+	
+	public void addAction(String name, Map<String, Object> params) {
+		ActionSearchResult result = new ActionSearchResult();
+		result.setName(name);
+		result.setParams(params);
+		results.add(result);
+	}
 }
